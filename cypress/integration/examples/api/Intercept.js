093d519010eaa360930bd71f2a0eee2938f29add
MarcinTester <marcintester19@gmail.com>
2024-05-05 15:17:19 +0200

    api test

--- cypress/integration/examples/api/Intercept.js
+++ cypress/integration/examples/api/Intercept.js
@@ -1,34 +1,44 @@
 /// <reference types="Cypress" />
+import HomePage from "../../../support/pages/HomePage";
+import LoginPage from "../../../support/pages/LoginPage";
+import ProductPage from "../../../support/pages/ProductPage";
 describe("API test", () => {
   let data;
+  const homePage = new HomePage();
+  const loginPage = new LoginPage();
+  const productPage = new ProductPage();
   before(function () {
     cy.fixture("testData").then(function (testData) {
       data = testData;
     });
   });
-  it("Bag test", function () {
-    cy.visit("https://automationteststore.com");
-    cy.intercept(
-      "GET",
-      "https://automationteststore.com/index.php?rt=r/product/product/addToCart"
-    ).as("addToCard");
-    // cy.intercept(
-    //   "POST",
-    //   "https://automationteststore.com/index.php?rt=r/checkout/cart/recalc_totals"
-    // ).as("recalc_totals");
-    cy.get('[class="active menu_home"]').trigger("mouseover");
-    cy.get('[class="menu_text"]').contains("Cart").click({ force: true });
-    cy.wait("@addToCard").should(({ request, response }) => {
+  it("search product", function () {
+    cy.visit(data.baseURL);
+    cy.intercept("GET", "https://automationexercise.com/products?search=top").as(
+      "searchProduct"
+    );
+    homePage.openProducts();
+    productPage.searchProducts(data.products[0])
+    cy.wait("@searchProduct").should(({ request, response }) => {
       cy.log("request: " + JSON.stringify(request));
       cy.log("response: " + JSON.stringify(response));
       expect(response.statusCode).to.eq(200);
-      cy.log("statusCode: " + response.statusCode);
+      cy.log("body: " + JSON.stringify(response.body));
     });
-    // cy.wait("@recalc_totals").should(({ request, response }) => {
-    //   cy.log("request: " + JSON.stringify(request));
-    //   cy.log("response: " + JSON.stringify(response));
-    //   expect(response.statusCode).to.eq(200);
-    //   cy.log("statusCode: " + response.statusCode);
-    // });
+  });
+
+  it("login", function () {
+    cy.visit(data.baseURL);
+    cy.intercept("POST", "https://automationexercise.com/login").as(
+      "login"
+    );
+    homePage.signIn();
+    loginPage.loginUser(data.email, data.password, data.username);
+    cy.wait("@login").should(({ request, response }) => {
+      cy.log("request: " + JSON.stringify(request.body));
+      cy.log("response: " + JSON.stringify(response));
+      expect(response.statusCode).to.eq(302);
+    });
   });
 });
+``
