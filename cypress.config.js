b500528325177270c193c167e44e2e5a38390899
MarcinTester <marcintester19@gmail.com>
2024-06-11 21:24:37 +0200

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require("cypress");
-const cypressSplit = require("cypress-split");
 module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
@@ -14,9 +13,6 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
-      cypressSplit(on, config);
-      // IMPORTANT: return the config object
-      return config;
     },
     video: true,
     specPattern: "cypress/integration/ui/*.js",
